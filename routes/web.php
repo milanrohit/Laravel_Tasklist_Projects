65969237c57290be67d9bee0dc21625b6111a80d
Milan Rohit <rohit.milan3@gmail.com>
2025-07-30 10:05:11 +0000

    feat: implement task creation and display functionality with new views

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 use App\Models\Task;
+use App\Models\User;
 
 /* Start project related routes */
 
@@ -9,12 +11,21 @@ Route::get('/', function () {
 });
 
 Route::get('/tasks', function (){
-    //return view('tasks.index', ['tasks' => \App\Models\Task::all()]);
-    return view('tasks.index', ['tasks' => \App\Models\Task::latest()->get()]);
+    return view('index', ['tasks' => \App\Models\Task::latest()->get()]);
 })->name('tasks.index');
 
+// Route to store a new task
+Route::get('/tasks/create', function () {
+    return view('create');
+})->name('tasks.create');
+
+
 Route::get('/tasks/{id}', function ($id) {
-    return view('tasks.show', ['tasks' => \App\Models\Task::findOrFail($id)]);
+    return view('show', ['tasks' => \App\Models\Task::findOrFail($id)]);
 })->name('tasks.show');
+
+Route::post('/tasks', function (Request $request) {
+    dd($request->all());
+})->name('tasks.store');
 
 ?>
